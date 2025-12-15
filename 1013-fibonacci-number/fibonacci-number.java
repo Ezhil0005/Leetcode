6eb30c24104c5c58283026e21b7a77a41df9766a
Ezhil0005 <ezhiloviyan9876@gmail.com>
2025-12-15 12:20:19 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (16.90%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int fib(int n) {
+        int a=0;
+        int b=1;
+        int temp=0;
+        if(n==0){
+        return 0;
+        }
+        for(int i=2;i<=n;i++)
+        {
+            temp=b;
+            b=a+b;
+            a=temp;
+        }
+        return b;
+    }
+}
