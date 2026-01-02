ff1e842184853132d3ce1d12f4fb88582d0f7216
Ezhil0005 <ezhiloviyan9876@gmail.com>
2026-01-02 11:46:45 +0530

    Time: 2 ms (10.94%) | Memory: 43.3 MB (6.76%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int count=0;
+      s= s.trim();
+        for(int i=0;i<s.length();i++)
+        {
+            
+            if(s.charAt(i)!=' ')
+            count++;
+            else
+            count=0;
+
+        }
+        return count;
+    }
+}
