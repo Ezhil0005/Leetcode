af338ea26c52b9d2a178a41dd414d588c4ebbd23
Ezhil0005 <ezhiloviyan9876@gmail.com>
2026-01-10 19:27:29 +0530

    Time: 1 ms (2.27%) | Memory: 44.8 MB (30.93%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==target)
+            {
+                return i;
+            }
+            else if(i<nums.length-1)
+            {
+                if(target>nums[i]&&target <nums[i+1])
+                  return i+1;
+            }
+
+          
+        }
+        if(target<nums[0])
+        return 0;
+        else
+        return nums.length;
+       
+    }
+}
