848c4623527a039ab8b16c02dd08e21d9b7c40e0
Ezhil0005 <ezhiloviyan9876@gmail.com>
2026-07-16 08:21:41 +0530

    Time: 5 ms (97.41%) | Memory: 42.7 MB (81.55%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,24 +1,35 @@
 class Solution {
     public int countSubstrings(String s) {
-        int left=0;
-        int right=0;
+        int odd=0;
+        int even=0;
+        int end=0;
+        int start=0;
         int count=0;
+
         for(int i=0;i<s.length();i++)
         {
             count+=sub(s,i,i);
             count+=sub(s,i,i+1);
+           int len= Math.max(odd,even);
+            if(len>end-start+1)
+            {
+                start=i-(len-1)/2;
+                end=i+len/2;
+            }
+
         }
         return count;
     }
     private int sub(String s,int left,int right)
     {
         int count=0;
-        while((left>=0&&right<s.length()&&s.charAt(left)==s.charAt(right)))
+        while((left>=0&&right<s.length())&&s.charAt(left)==s.charAt(right))
         {
-            count++;
             left--;
             right++;
+            count++;
         }
         return count;
+        // return right-left-1;
     }
-}
+    }
